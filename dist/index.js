8af5de1bde59e3909da6329e545330d98683fae3
abzal <abzal008@mail.ru>
2023-02-25 05:56:03 +0600

    add socet io

--- dist/index.js
+++ dist/index.js
@@ -7,11 +7,26 @@ const express_1 = __importDefault(require("express"));
 const http_1 = __importDefault(require("http"));
 const config_1 = require("./main/config");
 const app_1 = require("./main/app");
-const routes_1 = require("./main/routes");
+const socket_io_1 = require("socket.io");
 const app = (0, express_1.default)();
 (0, app_1.appUse)(app);
-(0, routes_1.routes)(app);
+// routes(app);
 const server = http_1.default.createServer(app);
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+const io = new socket_io_1.Server(server);
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.broadcast.emit('hi');
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); // This will emit the event to all connected sockets
 const port = process.env.PORT || config_1.PORT;
 server.listen(port, () => {
     console.log("Listening on port: " + port);
--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,35 @@ import http from "http";
 import {PORT} from "./main/config";
 import {appUse} from "./main/app";
 import {routes} from "./main/routes";
+import {Server} from "socket.io";
 
 const app = express();
 appUse(app);
-routes(app);
+// routes(app);
 
 const server = http.createServer(app);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+
+const io = new Server(server);
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.broadcast.emit('hi');
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); // This will emit the event to all connected sockets
+
+
 
 const port = process.env.PORT || PORT;
 
